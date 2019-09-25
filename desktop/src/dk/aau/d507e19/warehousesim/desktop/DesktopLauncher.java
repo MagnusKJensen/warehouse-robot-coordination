10eafb249ef5f58d647154ebd6c70028bd914c54
Magnus K. Jensen <magnjensen@gmail.com>
2019-09-25 10:52:47 +0200

    Initial commit

---
+++ desktop/src/dk/aau/d507e19/warehousesim/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package dk.aau.d507e19.warehousesim.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import dk.aau.d507e19.warehousesim.Simulation;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Simulation(), config);
+	}
+}
