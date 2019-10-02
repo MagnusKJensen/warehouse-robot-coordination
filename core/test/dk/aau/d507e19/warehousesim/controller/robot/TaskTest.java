2bf20fac3bbe62a268d448decdc44a94cce55929
Magnus K. Jensen <magnjensen@gmail.com>
2019-10-02 14:29:14 +0200

    Added tests for paths in the Task class

--- core/test/dk/aau/d507e19/warehousesim/controller/robot/TaskTest.java
+++ core/test/dk/aau/d507e19/warehousesim/controller/robot/TaskTest.java
@@ -15,25 +15,61 @@ public class TaskTest {
         ArrayList<GridCoordinate> expectedStrippedCoordinates = new ArrayList<>();
 
         // Full path
-        Collections.addAll(allCoordinates, new GridCoordinate(1, 5),
-                new GridCoordinate(1, 6),
-                new GridCoordinate(1, 7),
-                new GridCoordinate(2, 7),
-                new GridCoordinate(2, 8),
-                new GridCoordinate(3, 7),
-                new GridCoordinate(4, 7),
-                new GridCoordinate(5, 7));
+        Collections.addAll(allCoordinates,
+                new GridCoordinate(1, 1),
+                new GridCoordinate(1, 2),
+                new GridCoordinate(1, 3),
+                new GridCoordinate(2, 3),
+                new GridCoordinate(2, 4),
+                new GridCoordinate(3, 4),
+                new GridCoordinate(4, 4),
+                new GridCoordinate(4, 3),
+                new GridCoordinate(4, 2),
+                new GridCoordinate(4, 1));
 
         // Only the corners
-        Collections.addAll(expectedStrippedCoordinates, new GridCoordinate(1, 5),
-                new GridCoordinate(1, 7),
-                new GridCoordinate(2, 7),
-                new GridCoordinate(2, 8),
-                new GridCoordinate(3, 7),
-                new GridCoordinate(5, 7));
+        Collections.addAll(expectedStrippedCoordinates,
+                new GridCoordinate(1, 3),
+                new GridCoordinate(2, 3),
+                new GridCoordinate(2, 4),
+                new GridCoordinate(4, 4),
+                new GridCoordinate(4, 1));
 
         Task task = new Task(allCoordinates, Action.NONE);
-        //assertEquals(expectedStrippedCoordinates, task.getStrippedPath());
+        assertEquals(expectedStrippedCoordinates, task.getStrippedPath());
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void removeAllButCornersZeroLenTest() {
+        ArrayList<GridCoordinate> allCoordinates = new ArrayList<>();
+        Task task = new Task(allCoordinates, Action.NONE);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void noncontinuousStraightPathTest() {
+        ArrayList<GridCoordinate> allCoordinates = new ArrayList<>();
+        allCoordinates.add(new GridCoordinate(1, 1));
+        allCoordinates.add(new GridCoordinate(2, 1));
+        allCoordinates.add(new GridCoordinate(4, 1));
+        Task task = new Task(allCoordinates, Action.NONE);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void noncontinuousCornerPathTest() {
+        ArrayList<GridCoordinate> allCoordinates = new ArrayList<>();
+        allCoordinates.add(new GridCoordinate(1, 1));
+        allCoordinates.add(new GridCoordinate(2, 1));
+        allCoordinates.add(new GridCoordinate(3, 1));
+        allCoordinates.add(new GridCoordinate(3, 3));
+        Task task = new Task(allCoordinates, Action.NONE);
+    }
+
+
+    public void removeAllButCornersOneLenTest() {
+        ArrayList<GridCoordinate> allCoordinates = new ArrayList<>();
+        allCoordinates.add(new GridCoordinate(0,0));
+        Task task = new Task(allCoordinates, Action.NONE);
+        assertTrue(task.getStrippedPath().isEmpty());
     }
 
     @Test
