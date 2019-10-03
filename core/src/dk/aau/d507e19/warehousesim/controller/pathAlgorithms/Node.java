c61d78d208b04d95b080f7a50939c7f49a03d7bd
Mads <mk96@live.dk>
2019-10-03 09:17:50 +0200

    Added RRT classes

---
+++ core/src/dk/aau/d507e19/warehousesim/controller/pathAlgorithms/Node.java
@@ -0,0 +1,44 @@
+package dk.aau.d507e19.warehousesim.controller.pathAlgorithms;
+
+import java.util.List;
+
+public class Node<T> {
+    private T data;
+    private Node<T> parent;
+    private List<Node<T>> children;
+
+    public Node(T data, Node<T> parent) {
+        this.data = data;
+        this.parent = parent;
+    }
+    public void addChild(Node<T> child){
+        children.add(child);
+    }
+    public void removeChild(Node<T> child){
+        if(children.contains(child)){
+            children.remove(child);
+        }
+    }
+    public List<Node<T>> getChildren() {
+        return children;
+    }
+
+    public boolean containsData(Node<T> node){
+        if(this.data.equals(node.data)){
+            //If this node is the node we're looking for then no need to traverse tree
+            System.out.println("The node we're looking for is the one we have(goal == start");
+            return true;
+        }
+        //if there are no children then return false
+        if(!children.isEmpty()){
+            for(Node<T> n : children){
+                if(n.data.equals(node.data)){
+                    return true;
+                }else{
+                    n.containsData(n);
+                }
+            }
+        }
+        return false;
+    }
+}
---
+++ core/src/dk/aau/d507e19/warehousesim/controller/pathAlgorithms/RRTPlanner.java
@@ -0,0 +1,35 @@
+package dk.aau.d507e19.warehousesim.controller.pathAlgorithms;
+
+import dk.aau.d507e19.warehousesim.Position;
+import dk.aau.d507e19.warehousesim.SimulationApp;
+import dk.aau.d507e19.warehousesim.WarehouseSpecs;
+import dk.aau.d507e19.warehousesim.controller.robot.Robot;
+
+public class RRTPlanner {
+
+    void generateRRTroute(Robot robot, Position destination){
+        boolean hasRoute = false;
+        Node<Position> Tree = new Node<Position>(robot.getCurrentPosition(),null);
+        //Run until a route is found
+        while(!hasRoute){
+            growRRT(Tree);
+            //traverse tree to see if destination is reachable
+            hasRoute = Tree.containsData(Tree);
+        }
+    }
+    private Node<Position> growRRT(Node<Position> tree){
+        //Generate a new random location using seeded random
+        Position randPos = new Position(
+                SimulationApp.random.nextInt(WarehouseSpecs.wareHouseWidth),
+                SimulationApp.random.nextInt(WarehouseSpecs.wareHouseHeight));
+        Node<Position> nearest = findNearestNeighbour(tree,randPos);
+        //Todo find a good way to add the new point to the tree
+        return tree;
+    }
+    private Node<Position> findNearestNeighbour(Node<Position> tree, Position randPos){
+        //find nearest neighbour
+        //Todo implement this
+        return null;
+    }
+
+}
