3d5bbd6045e90b8cc32ce0577fb346ea2e1914db
Hannah <hannah97@live.dk>
2019-10-03 11:15:59 +0200

    testy bois

--- core/src/dk/aau/d507e19/warehousesim/controller/pathAlgorithms/Astar.java
+++ core/src/dk/aau/d507e19/warehousesim/controller/pathAlgorithms/Astar.java
@@ -3,74 +3,120 @@ package dk.aau.d507e19.warehousesim.controller.pathAlgorithms;
 import java.util.ArrayList;
 
 public class Astar {
-    private int gridLength = 10;
-    private Tile grid[][] = new Tile[gridLength][gridLength];
-    private int xEndposition = 0;
-    private int yEndposition = 4;
-    private int xStartposition = 0;
-    private int yStartposition = 0;
+
+    private Tile[][] grid;
+    private int xEndposition;
+    private int yEndposition;
+    private int xStartposition;
+    private int yStartposition;
 
     ArrayList<Tile> openList = new ArrayList<>();
     ArrayList<Tile> closedList = new ArrayList<>();
     private Tile currentTile;
     private Tile previousTile;
 
+    public Astar(Tile[][] grid, int xStart, int yStart, int xEnd, int yEnd) {
+        this.grid = fillGrid(grid);
+        this.xStartposition = xStart;
+        this.yStartposition = yStart;
+        this.xEndposition = xEnd;
+        this.yEndposition = yEnd;
+    }
+
     public static void main(String[] args) {
-        Astar astar = new Astar();
-        astar.fillGrid();
+        // Sets grid size
+        int gridLength = 10;
+
+        // Makes new grid
+        Tile[][] grid = new Tile[gridLength][gridLength];
+
+        // Makes new Astar object and fills grid
+        Astar astar = new Astar(grid, 0, 0,0,4);
+
+        // Adds the starting tile to closed list.
         astar.addStartTileToClosedList();
-        astar.currentTile = astar.closedList.get(astar.closedList.size() - 1);
+
+        // Calculates the optimal A* path
         astar.calculatePath();
 
     }
 
-    public void fillGrid() {
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
+    public Tile[][] fillGrid(Tile[][] grid) {
+
+        // Fills grid with tiles matching the coordinates
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid.length; j++) {
                 grid[i][j] = new Tile(i, j);
 
             }
         }
+        return grid;
     }
 
     public void addStartTileToClosedList() {
 
+        // Adds startTile to closedList
         closedList.add(grid[xStartposition][yStartposition]);
+
+        // Blocks startTile so that it cannot be used anymore
         grid[xStartposition][yStartposition].setBlocked(true);
+
+        // Sets currentTile to the top tile in closedList (startTile)
+        currentTile = closedList.get(closedList.size() - 1);
+
+    }
+
+    public void calculatePath() {
+
+        // While is true if the currentTile does not have the same x coordinate and the same y coordinate as the end Tile.
+        while (!(currentTile.getCurrentXPosition() == xEndposition && currentTile.getCurrentYPosition() == yEndposition)) {
+
+            // If the closedList has more than one tile, then set previousTile
+            // Previous tile is the last in closedList
+            if (closedList.size() > 2) {
+                previousTile = closedList.get(closedList.size() - 2);
+                currentTile.setPreviousXposition(previousTile.getCurrentXPosition());
+                currentTile.setPreviousYposition(previousTile.getCurrentYPosition());
+            }
+
+            // Add the valid tiles to openList
+            addTilesToOpenList();
+
+            // Add the lowest cost tile to closedList
+            addTilesToClosedList();
+
+            // CurrentTile is now the top tile in closedList
+            currentTile = closedList.get(closedList.size() - 1);
+        }
     }
 
     public void addTilesToOpenList() {
 
-        if (!(currentTile.getCurrentYPosition() - 1 < 0 || grid[currentTile.getCurrentXPosition()][currentTile.getCurrentYPosition() - 1].isBlocked() == true)) {
-            grid[currentTile.getCurrentYPosition() - 1][currentTile.getCurrentXPosition()].setPreviousYposition(currentTile.getCurrentYPosition());
-            grid[currentTile.getCurrentYPosition() - 1][currentTile.getCurrentXPosition()].setPreviousXposition(currentTile.getCurrentXPosition());
-            grid[currentTile.getCurrentYPosition() + 1][currentTile.getCurrentXPosition()].setBlocked(true);
+        // If the current tiles neighbor (x,y-1) is valid, and not blocked (in closedList)
+        if (!(currentTile.getCurrentYPosition() - 1 < 0 || grid[currentTile.getCurrentXPosition()][currentTile.getCurrentYPosition() - 1].isBlocked())) {
+
+
+            grid[currentTile.getCurrentXPosition()][currentTile.getCurrentXPosition()].setBlocked(true);
             if(!(openList.contains(grid[currentTile.getCurrentYPosition() - 1][currentTile.getCurrentXPosition()])|| closedList.contains(grid[currentTile.getCurrentYPosition()-1][currentTile.getCurrentXPosition()])))
             openList.add(grid[currentTile.getCurrentYPosition() - 1][currentTile.getCurrentXPosition()]);
 
         }
-        if (!(currentTile.getCurrentYPosition() + 1 > 10 || grid[currentTile.getCurrentXPosition()][currentTile.getCurrentYPosition() + 1].isBlocked() == true)) {
-            grid[currentTile.getCurrentYPosition() + 1][currentTile.getCurrentXPosition()].setPreviousYposition(currentTile.getCurrentYPosition());
-            grid[currentTile.getCurrentYPosition() + 1][currentTile.getCurrentXPosition()].setPreviousXposition(currentTile.getCurrentXPosition());
+        if (!(currentTile.getCurrentYPosition() + 1 > 10 || grid[currentTile.getCurrentXPosition()][currentTile.getCurrentYPosition() + 1].isBlocked())) {
             grid[currentTile.getCurrentYPosition() + 1][currentTile.getCurrentXPosition()].setBlocked(true);
             if(!(openList.contains(grid[currentTile.getCurrentYPosition() + 1][currentTile.getCurrentXPosition()])|| closedList.contains(grid[currentTile.getCurrentYPosition()+1][currentTile.getCurrentXPosition()])))
             openList.add(grid[currentTile.getCurrentYPosition() + 1][currentTile.getCurrentXPosition()]);
 
 
         }
-        if (!(currentTile.getCurrentXPosition() - 1 < 0 || grid[currentTile.getCurrentXPosition() - 1][currentTile.getCurrentYPosition()].isBlocked() == true)) {
-            grid[currentTile.getCurrentYPosition()][currentTile.getCurrentXPosition() - 1].setPreviousYposition(currentTile.getCurrentYPosition());
-            grid[currentTile.getCurrentYPosition()][currentTile.getCurrentXPosition() - 1].setPreviousXposition(currentTile.getCurrentXPosition());
+        if (!(currentTile.getCurrentXPosition() - 1 < 0 || grid[currentTile.getCurrentXPosition() - 1][currentTile.getCurrentYPosition()].isBlocked())) {
             grid[currentTile.getCurrentYPosition()][currentTile.getCurrentXPosition() - 1].setBlocked(true);
             if(!(openList.contains(grid[currentTile.getCurrentYPosition()][currentTile.getCurrentXPosition() - 1]) || closedList.contains(grid[currentTile.getCurrentYPosition()][currentTile.getCurrentXPosition() -1])))
                 openList.add(grid[currentTile.getCurrentYPosition()][currentTile.getCurrentXPosition() - 1]);
 
 
         }
-        if (!(currentTile.getCurrentXPosition() + 1 > 10 || grid[currentTile.getCurrentXPosition() + 1][currentTile.getCurrentYPosition()].isBlocked() == true)) {
+        if (!(currentTile.getCurrentXPosition() + 1 > 10 || grid[currentTile.getCurrentXPosition() + 1][currentTile.getCurrentYPosition()].isBlocked())) {
             grid[currentTile.getCurrentYPosition()][currentTile.getCurrentXPosition() + 1].setBlocked(true);
-            grid[currentTile.getCurrentYPosition()][currentTile.getCurrentXPosition() + 1].setPreviousYposition(currentTile.getCurrentYPosition());
-            grid[currentTile.getCurrentYPosition()][currentTile.getCurrentXPosition() + 1].setPreviousXposition(currentTile.getCurrentXPosition());
             if(!(openList.contains(grid[currentTile.getCurrentYPosition()][currentTile.getCurrentXPosition() + 1]) || closedList.contains(grid[currentTile.getCurrentYPosition()][currentTile.getCurrentXPosition() + 1])))
             openList.add(grid[currentTile.getCurrentYPosition()][currentTile.getCurrentXPosition() + 1]);
 
@@ -98,17 +144,6 @@ public class Astar {
 
     }
 
-
-    public void calculatePath() {
-        while (!(currentTile.getCurrentXPosition() == xEndposition && currentTile.getCurrentYPosition() == yEndposition)) {
-            currentTile = closedList.get(closedList.size() - 1);
-            if (closedList.size() > 2)
-                previousTile = closedList.get(closedList.size() - 2);
-
-            addTilesToOpenList();
-            addTilesToClosedList();
-        }
-    }
 
 
 }
