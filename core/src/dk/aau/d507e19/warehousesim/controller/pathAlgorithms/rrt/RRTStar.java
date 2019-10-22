d328a17b6e58211bbd19fe693718349bf9ea8e3f
Mads <mk96@live.dk>
2019-10-22 13:32:37 +0200

    added rewire() and helper methods

--- core/src/dk/aau/d507e19/warehousesim/controller/pathAlgorithms/rrt/RRTStar.java
+++ core/src/dk/aau/d507e19/warehousesim/controller/pathAlgorithms/rrt/RRTStar.java
@@ -22,17 +22,56 @@ public class RRTStar extends RRTBase {
 
     @Override
     protected void growUntilPathFound(GridCoordinate destination){
-        //growUntilAllNodesFound();
-        boolean foundPath = false;
         //Run until a route is found
-        while (!foundPath) {
+        while (!isFullyExplored()) {
             //grow tree by one each time(maybe inefficient?)
             growRRT(root, 1);
             //every time a new node is added, check to see if we can improve it
             improvePath(latestNode.getData());
             //check if any of the nodes in the vicinity can be rewired to reduce their cost
-            //rewire(latestNode);
-            foundPath = doesNodeExist(destination);
+            rewire(latestNode);
         }
+    }
+
+    private void rewire(Node<GridCoordinate> node){
+        //find potential nodes that could have their paths improved.
+        //for each one, check if their path would be better if node was their parent
+        ArrayList<Node<GridCoordinate>> neighbours = trimImprovementsList(findNodesInRadius(node.getData(),1),node.getData());
+        for(Node<GridCoordinate> n : neighbours){
+            if(canBeRewired(node,n)){
+                //make copyTree
+                Node<GridCoordinate> copyRoot = root.copy();
+                //set copy of node to be parent of copy of n
+                Node<GridCoordinate> copyN = copyRoot.findNode(n.getData());
+                copyN.setParent(copyRoot.findNode(node.getData()));
+                //check if the cost of real tree to n is worse than copy version
+                if(cost(n) > cost(copyN)){
+                    //if n is worse copy, we make the copy a reality
+                    n.setParent(node);
+                }
+            }
+        }
+    }
+    private boolean canBeRewired(Node<GridCoordinate> node, Node<GridCoordinate> n){
+        if(n.getParent() == null){
+            return false;
+        } else if (n.getParent().equals(node)) {
+            return false;
+        } else if(n.getParent().equals(root)){
+            return false;
+        } else if(node.getParent().equals(n)){
+            return false;
+        }else if (isInPathToRoot(node,n)){
+            return false;
+        }else return true;
+    }
+    private boolean isInPathToRoot(Node<GridCoordinate> node, Node<GridCoordinate> n){
+        if(node.getParent() == null){
+            return false;
+        }
+        if(node.getParent().equals(n)){
+            return true;
+        }
+        return isInPathToRoot(node.getParent(),n);
     }
 }
