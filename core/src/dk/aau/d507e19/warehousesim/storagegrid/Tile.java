f03f79931e277f8d5a47ff88b7c3a157ac849440
Mads <mk96@live.dk>
2019-10-24 12:51:23 +0200

    added renderTreeNode method. Used to render each individual node in a RRT tree

--- core/src/dk/aau/d507e19/warehousesim/storagegrid/Tile.java
+++ core/src/dk/aau/d507e19/warehousesim/storagegrid/Tile.java
@@ -5,9 +5,11 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
 import dk.aau.d507e19.warehousesim.Drawable;
 import dk.aau.d507e19.warehousesim.GraphicsManager;
 import dk.aau.d507e19.warehousesim.Position;
+import dk.aau.d507e19.warehousesim.controller.pathAlgorithms.rrt.Node;
 import dk.aau.d507e19.warehousesim.controller.robot.GridCoordinate;
 
 public class Tile implements Drawable {
@@ -62,6 +64,24 @@ public class Tile implements Drawable {
 
     public void renderOverlay(ShapeRenderer shapeRenderer, Color color){
         renderFilledCenter(shapeRenderer, color);
+    }
+
+    public void renderTreeNode(Node<GridCoordinate> node, ShapeRenderer shapeRenderer, Color color){
+        Vector2 start = new Vector2(posX + 0.5f*TILE_SIZE,posY+ 0.5f*TILE_SIZE);
+        Gdx.gl.glLineWidth(3);
+        shapeRenderer.setColor(color);
+        if(node.getParent()== null){
+            shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+            shapeRenderer.circle(start.x,start.y,0.3f);
+        }else{
+            shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+            Vector2 end = new Vector2(
+                    node.getParent().getData().getX()+0.5f*TILE_SIZE,
+                    node.getParent().getData().getY()+0.5f*TILE_SIZE);
+
+            shapeRenderer.line(start,end);
+        }
+        shapeRenderer.end();
     }
 
     public int getPosX() {
