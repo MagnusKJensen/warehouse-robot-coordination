6a0539594fbb53dd006dad730da048b20c1c166c
Mads <mk96@live.dk>
2019-10-31 14:35:04 +0100

    fixed reference issues

--- core/src/dk/aau/d507e19/warehousesim/controller/pathAlgorithms/rrt/RRTStar.java
+++ core/src/dk/aau/d507e19/warehousesim/controller/pathAlgorithms/rrt/RRTStar.java
@@ -101,17 +101,17 @@ public class RRTStar extends RRTBase {
     public void attemptOptimise(){
         //create a copy of the tree
         Node<GridCoordinate> copyRoot = root.copy();
-        Node<GridCoordinate> backupCpy = copyRoot;
-        Node<GridCoordinate> destination = copyRoot.findNode(destinationNode.getData());
+        ArrayList<Node<GridCoordinate>> nodesInPath = getNodesInPath(copyRoot.findNode(destinationNode.getData()));
+        Node<GridCoordinate> backupCpy = root.copy();
         //then call optimise on the copy, if copy is better then root = copy
         //we check for every node in the path starting from root, if we find a optimization we end the loop immediately
-            for(int i=0; i < getNodesInPath(destination).size(); i++){
-                if(optimise(getNodesInPath(destination).get(i),destination)){
+            for(int i=0; i <nodesInPath.size(); i++){
+                if(optimise(nodesInPath.get(i),copyRoot.findNode(destinationNode.getData()))){
                     //check if it is actually faster
-                    if(cost(destination) < cost(destinationNode)){
+                    if(cost(copyRoot.findNode(destinationNode.getData())) < cost(destinationNode)){
                        // possibleOptimisations.add(copyRoot);
                         root = copyRoot;
-                        destinationNode = destination;
+                        destinationNode = copyRoot.findNode(destinationNode.getData());
                         path = makePath(destinationNode);
                         updateAllNodes(root);
                         break;
