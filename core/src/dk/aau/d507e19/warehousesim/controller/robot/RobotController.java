c01247dae96172e36f186ab50c2aad88f73f0a33
Mads Bau <mk96@live.dk>
2019-11-06 20:01:20 +0100

    added skeleton methods for reactive collision handling

--- core/src/dk/aau/d507e19/warehousesim/controller/robot/RobotController.java
+++ core/src/dk/aau/d507e19/warehousesim/controller/robot/RobotController.java
@@ -13,9 +13,11 @@ import dk.aau.d507e19.warehousesim.controller.server.Server;
 import dk.aau.d507e19.warehousesim.controller.server.TimeFrame;
 import dk.aau.d507e19.warehousesim.exception.DoubleReservationException;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Optional;
+import java.util.function.Function;
 
 public class RobotController {
     private Server server;
@@ -99,7 +101,6 @@ public class RobotController {
         return tasks;
     }
 
-
     public boolean requestMove(){
 
         if(robot.getCurrentStatus() == Status.BUSY){
@@ -115,6 +116,44 @@ public class RobotController {
     private boolean interruptCurrentTask() {
         if(tasks.isEmpty()) return true;
         return tasks.getFirst().interrupt();
+    }
+
+    private void checkForCollisions(){
+        //find nearby robots
+        ArrayList<Robot> nearbyRobots = scanForNearbyRobots();
+        if(!nearbyRobots.isEmpty()){
+            for(Robot r : nearbyRobots){
+                if(goingToCollide(r.getRobotController())){
+                    handleCollision();
+                }
+            }
+        }
+    }
+
+    private ArrayList<Robot> scanForNearbyRobots(){
+        ArrayList<Robot> nearbyRobots = new ArrayList<>();
+        //more readable
+        Function<GridCoordinate,Double> distance = gc -> Math.sqrt(Math.pow(gc.getX() - robot.getGridCoordinate().getX(), 2) + Math.pow(gc.getY() - robot.getGridCoordinate().getY(), 2));
+        for(Robot r: server.getAllRobots()){
+            if(distance.apply(r.getGridCoordinate()) <= 2){
+                if(!r.equals(robot)){
+                    nearbyRobots.add(r);
+                }
+            }
+        }
+        return nearbyRobots;
+    }
+
+    private boolean goingToCollide(RobotController them){
+        //Check if paths have any of the same coordinates
+        //If they do, then check if they are in the same timeframe
+        //How: Get reservation lists for both, then check the timeframe for the potential collision coordinate
+        //If they will collide, return true
+        return false;
+    }
+
+    private void handleCollision(){
+
     }
 
 }
