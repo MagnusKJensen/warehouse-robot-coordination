1e05c99f1017b3abbb425f7f16ee62e9dd028077
Mads <mk96@live.dk>
2019-11-08 19:06:50 +0100

    Paths are a lot better now, still need to fix big zig zags

--- core/src/dk/aau/d507e19/warehousesim/controller/pathAlgorithms/rrt/RRTStarExtended.java
+++ core/src/dk/aau/d507e19/warehousesim/controller/pathAlgorithms/rrt/RRTStarExtended.java
@@ -1,15 +1,10 @@
 package dk.aau.d507e19.warehousesim.controller.pathAlgorithms.rrt;
-
-import dk.aau.d507e19.warehousesim.SimulationApp;
-import dk.aau.d507e19.warehousesim.controller.path.Path;
-import dk.aau.d507e19.warehousesim.controller.path.Step;
 import dk.aau.d507e19.warehousesim.controller.robot.GridCoordinate;
 import dk.aau.d507e19.warehousesim.controller.robot.RobotController;
-
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 
 public class RRTStarExtended extends RRTStar {
+    String currentDirection = "Still";
     public RRTStarExtended(RobotController robotController) {
         super(robotController);
     }
@@ -20,87 +15,14 @@ public class RRTStarExtended extends RRTStar {
         //optimise with distance first
         //then optimise with time
         optimalPathOptimise(destinationNode);
-            //System.out.println("DID NOT FIND OPTIMAL");
-            //smartOptimiseTime(destinationNode);
         path = makePath(destinationNode);
-
-    }
-    public void smartOptimiseDistance(Node<GridCoordinate> node){
-        if (node.getParent() != null) {
-            for(Node<GridCoordinate> n : trimImprovementsList(findNodesInRadius(node.getData(),1),node.getData())){
-                if(n.equals(node.getParent()) || node.getParent().equals(root)){
-                    continue;
-                }
-                if(!canBeRewired(n,node)){
-                    continue;
-                }
-                if(n.equals(root)){
-                    node.setParent(n);
-                }else{
-                    if(distance(n.getData(),root.getData()) < distance(node.getParent().getData(),root.getData()) ){
-                        node.setParent(n);
-                    }
-                }
-            }
-            smartOptimiseDistance(node.getParent());
-        }
-    }
-
-    public void smartOptimiseTime(Node<GridCoordinate> node){
-        if (node.getParent() != null) {
-            for(Node<GridCoordinate> n : trimImprovementsList(findNodesInRadius(node.getData(),1),node.getData())){
-                if(n.equals(node.getParent()) || node.getParent().equals(root)){
-                    continue;
-                }
-                if(!canBeRewired(n,node)){
-                    if(!hasBetterParent(n)){
-                        continue;
-                    }
-                }
-                if(n.equals(root)){
-                    node.setParent(n);
-                }else{
-                    //parentOptimiser(n);
-                    //might change node's parent to root so check to prevent
-                    if(node.getParent().equals(root)){
-                        continue;
-                    }
-                    if(cost(n) < cost(node.getParent()) ){
-                        node.setParent(n);
-                    }
-                }
-            }
-            smartOptimiseTime(node.getParent());
-        }
-    }
-    private void parentOptimiser(Node<GridCoordinate> n){
-        ArrayList<Node<GridCoordinate>> neighbours = trimImprovementsList(findNodesInRadius(n.getData(),1),n.getData());
-        if(neighbours.contains(root)){
-            n.setParent(root);
-        }
-        for(Node<GridCoordinate> pn: neighbours){
-            if(n.getParent().equals(root)){
-                continue;
-            }
-            if(!canBeRewired(pn,n)){
-                continue;
-            }
-            if(pn.equals(n.getParent())){
-                parentOptimiser(pn);
-            }
-            if(pn.equals(root)){
-                n.setParent(pn);
-            }else{
-                if(distance(pn.getData(),root.getData()) < distance(n.getParent().getData(),root.getData())){
-                    n.setParent(pn);
-                    //parentOptimiser(pn);
-                }
-            }
-        }
     }
     private boolean hasBetterParent(Node<GridCoordinate> node){
         for(Node<GridCoordinate> n: trimImprovementsList(findNodesInRadius(node.getData(),1),node.getData())){
             if(node.getParent().equals(n)){
+                if(distance(node.getParent().getData(),root.getData()) < distance(node.getData(),root.getData())){
+                    return true;
+                }
                 continue;
             }
             if(distance(n.getData(),root.getData()) < distance(node.getParent().getData(),root.getData())){
@@ -111,45 +33,71 @@ public class RRTStarExtended extends RRTStar {
     }
     public boolean optimalPathOptimise(Node<GridCoordinate> node){
         ArrayList<Node<GridCoordinate>> neighbours = trimImprovementsList(findNodesInRadius(node.getData(),1),node.getData());
+        ArrayList<Node<GridCoordinate>> bestNeighbours = new ArrayList<>();
+        Node<GridCoordinate> bestNeighbour=null;
         if(neighbours.contains(root)){
             node.setParent(root);
-            System.out.println("MADE OPTIMAL PATH");
             return true;
         }
-        if(destinationNode.getData().equals(new GridCoordinate(19,2)) && node.getData().equals(new GridCoordinate(8,2))){
-            System.out.println();
-        }
+        //Find all neighbours that could be good
         for(Node<GridCoordinate> neighbour : neighbours) {
             if (!canBeRewired(neighbour, node)) {
-                continue;
+                if(!hasBetterParent(neighbour)){
+                    continue;
+                }
+
             }
-            //if n is on the same axis as root and its distance is closer then make n new parent
-            if (neighbour.getData().getX() == root.getData().getX() || neighbour.getData().getY() == root.getData().getY()) {
-                if (distance(neighbour.getData(), root.getData()) < distance(node.getData(), root.getData())) {
-                    node.setParent(neighbour);
-                    return optimalPathOptimise(neighbour);
+            //if n to root is the same direction as neighbour to root i.e we dont have to turn add it to potential neighbours list
+            if(calcDirection(node.getData(),root.getData()).equals(calcDirection(neighbour.getData(),root.getData()))){
+                bestNeighbours.add(neighbour);
+            }
+            //if neighbour is on the same x or y axis as root, we should add it as well
+            else if(neighbour.getData().getX() == root.getData().getX() ||neighbour.getData().getY() == root.getData().getY()){
+                bestNeighbours.add(neighbour);
+            }
+        }
+        if(!bestNeighbours.isEmpty()){
+            //find best of best
+            //if we have one on the same x or y axis as root, then thats the best
+            for(Node<GridCoordinate> candidate : bestNeighbours){
+                if(candidate.getData().getX() == root.getData().getX() || candidate.getData().getY() == root.getData().getY()){
+                    //we might have two of these so we check to make sure we get the closest one
+                    if(bestNeighbour==null||distance(candidate.getData(),root.getData()) < distance(bestNeighbour.getData(),root.getData())){
+                        bestNeighbour = candidate;
+                    }
+                }
+            }
+            if(bestNeighbour==null){
+                //if its null, that means that we did not have any neighbours that were on the same axis as root. In this case we pick the one thats closest to root
+                for(Node<GridCoordinate> candidate : bestNeighbours){
+                    if(bestNeighbour==null || distance(candidate.getData(),root.getData()) <= distance(bestNeighbour.getData(),root.getData())){
+                        if(bestNeighbour == null){
+                            bestNeighbour = candidate;
+                        }else if(distance(candidate.getData(),root.getData()) == distance(bestNeighbour.getData(),root.getData())){
+                            //we should check that the direction from nodes child in the path to node is the same as node to candidate
+                            //This way we ensure that we always choose the straight paths
+                            if(currentDirection.equals(calcDirection(node.getData(),candidate.getData()))){
+                                bestNeighbour=candidate;
+                            }
+                        }else bestNeighbour = candidate;
+
+                    }
                 }
             }
         }
-        for(Node<GridCoordinate> neighbour : neighbours) {
-            if (!canBeRewired(neighbour, node)) {
-                continue;
-            }
-            //if n is on the same axis as root and its distance is closer then make n new parent
-            if(calcDirection(node.getData(),root.getData()).equals(calcDirection(neighbour.getData(),root.getData()))){
-
-                node.setParent(neighbour);
-                return optimalPathOptimise(neighbour);
-            }
+        if(bestNeighbour!=null){
+            currentDirection = calcDirection(node.getData(),bestNeighbour.getData());
+            node.setParent(bestNeighbour);
+            return optimalPathOptimise(bestNeighbour);
         }
         return false;
-
     }
+    /**
+     * Overridden to ensure tree is fully grown as the optimise assumes that tree is fully grown
+     * */
     @Override
     protected void growUntilPathFound(GridCoordinate destination) {
         //grow tree fully to ensure perfect paths todo change to only generate within bounds
         growUntilFullyExplored();
     }
-
-
 }
