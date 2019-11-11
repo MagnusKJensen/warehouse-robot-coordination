a798564b0a68ffa7c95c4dac54c72288ae400bb6
mads faber <madsfaber111@live.dk>
2019-11-11 13:27:30 +0100

    add comentary for astartile

--- core/test/dk/aau/d507e19/warehousesim/controller/pathAlgorithms/aStar/AStarTileTest.java
+++ core/test/dk/aau/d507e19/warehousesim/controller/pathAlgorithms/aStar/AStarTileTest.java
@@ -13,6 +13,7 @@ public class AStarTileTest {
     @Test
     public void setPreviousXPosition() {
         aStarTile = new AStarTile(1, 1);
+        //Sets the previus x postion that was used by Astar to calculate path
         aStarTile.setPreviousXPosition(0);
         assertEquals(aStarTile.getPreviousXPosition(), 0);
     }
@@ -20,6 +21,7 @@ public class AStarTileTest {
     @Test
     public void setPreviousYPosition() {
         aStarTile = new AStarTile(1, 1);
+        //Sets the previus y postion that was used by Astar to calculate path
         aStarTile.setPreviousYPosition(0);
 
         assertEquals(aStarTile.getGetPreviousYPosition(), 0);
@@ -28,14 +30,15 @@ public class AStarTileTest {
     @Test
     public void setBlocked() {
         aStarTile = new AStarTile(1, 1);
+        //blocks astar tile wich meeans it cant be accesed by astar
         aStarTile.setBlocked(true);
-
         assertEquals(aStarTile.isBlocked(), true);
     }
 
     @Test
     public void setG() {
         aStarTile = new AStarTile(1, 1);
+        //sets g witch is the steps taken by astar
         aStarTile.setG(1);
 
         assertEquals(aStarTile.getG(), 1);
@@ -72,6 +75,7 @@ public class AStarTileTest {
         aStarTile = new AStarTile(1, 1);
         aStarTile.calculateG(0);
         aStarTile.calculateH(1,2);
+        // calculates F based on H and G
         aStarTile.calculateF();
         assertEquals(aStarTile.getF(), 2);
     }
@@ -88,6 +92,7 @@ public class AStarTileTest {
     @Test
     public void calculateH() {
         aStarTile = new AStarTile(1, 1);
+        //calculates hueristic path to final destination
         aStarTile.calculateH(1,2);
         assertEquals(aStarTile.getH(), 1);
     }
@@ -95,6 +100,7 @@ public class AStarTileTest {
     @Test
     public void calculateG() {
         aStarTile = new AStarTile(1, 1);
+        //increments g with 1 to simulate a step taken
         aStarTile.calculateG(0);
         assertEquals(aStarTile.getG(), 1);
     }
@@ -102,8 +108,11 @@ public class AStarTileTest {
     @Test
     public void calculateF() {
         aStarTile = new AStarTile(1, 1);
+
         aStarTile.calculateG(0);
+        //calculates hueristic path to final destination
         aStarTile.calculateH(1,2);
+        // calculates F based on H and G
         aStarTile.calculateF();
         assertEquals(aStarTile.getF(), 2);
     }
