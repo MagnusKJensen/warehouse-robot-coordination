e6f40231662f48c4701c56c597bff6c9065124a9
Mads <mk96@live.dk>
2019-11-13 16:48:51 +0100

    small updates

--- core/src/dk/aau/d507e19/warehousesim/controller/robot/controlsystems/ControlSystemManager.java
+++ core/src/dk/aau/d507e19/warehousesim/controller/robot/controlsystems/ControlSystemManager.java
@@ -1,22 +1,45 @@
 package dk.aau.d507e19.warehousesim.controller.robot.controlsystems;
 
+import dk.aau.d507e19.warehousesim.controller.robot.RobotController;
+
 import java.util.ArrayList;
 
 public class ControlSystemManager {
+    RobotController robotController;
     ArrayList<Sensor> sensors = new ArrayList<>();
 
-    public ControlSystemManager() {
+    public ControlSystemManager(RobotController robotController) {
+        this.robotController = robotController;
         initSensors();
     }
     private void initSensors(){
         //init our sensors
+        sensors.add(new MechanicalSensor(SensorState.NOMINAL,robotController));
+        sensors.add(new Battery(SensorState.NOMINAL,robotController));
     }
 
     public void checkSystem(){
         for(Sensor s : sensors){
+            s.update();
             if(s.getState() == SensorState.FAILURE){
                 s.handleFailure();
             }
         }
+    }
+    public Battery getBattery(){
+        for(Sensor s : sensors){
+            if(s instanceof Battery){
+                return (Battery) s;
+            }
+        }
+        throw new RuntimeException("ERROR: Battery not found in sensors");
+    }
+    public MechanicalSensor getMechanicalSensor(){
+        for(Sensor s : sensors){
+            if(s instanceof MechanicalSensor){
+                return (MechanicalSensor) s;
+            }
+        }
+        throw new RuntimeException("ERROR: MechanicalSensor not found in sensors");
     }
 }
