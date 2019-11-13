488117c830e5ff680ab31491faf4ec0972f7a900
Mads <mk96@live.dk>
2019-11-13 10:38:03 +0100

    new tile where robots can enter maintenance

---
+++ core/src/dk/aau/d507e19/warehousesim/storagegrid/MaintenanceTile.java
@@ -0,0 +1,7 @@
+package dk.aau.d507e19.warehousesim.storagegrid;
+
+public class MaintenanceTile extends Tile {
+    public MaintenanceTile(int posX, int posY) {
+        super(posX, posY);
+    }
+}
