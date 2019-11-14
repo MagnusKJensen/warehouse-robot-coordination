96da30c36b717960faba7e3db3c774f4b05bf1ff
Mads <mk96@live.dk>
2019-11-14 13:11:14 +0100

    added textures for when battery is dry

--- core/src/dk/aau/d507e19/warehousesim/GraphicsManager.java
+++ core/src/dk/aau/d507e19/warehousesim/GraphicsManager.java
@@ -69,6 +69,8 @@ public class GraphicsManager {
         addTexture("Simulation/Robots/chargingRobot.png");
         addTexture("Simulation/Robots/repairRobot.png");
         addTexture("Simulation/Robots/repairRobot_RED.png");
+        addTexture("Simulation/Robots/whiteRobot.png");
+        addTexture("Simulation/Robots/whiteRobot_X.png");
 
         // Tiles
         addTexture("Simulation/tiles/Bin.png");
